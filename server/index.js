406f7b0296f5190352f12a8a8bc9a3294e5349f7
AJAFF7 <a.j.s-engineer@outlook.com>
2025-01-14 17:24:57 +0300

    Updated dependencies and package configuration

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import { userRouter } from "./routes/personals-All.js";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import path from 'path'; // Add this import
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,30 +14,32 @@ const app = express();
 
 app.use(express.json());
 
-
-//app.use(cors());
-
+// Enable CORS for all origins
 app.use(cors({
     origin: '*', // Allow all origins
 }));
 
-
-
-app.use(cors());
+// Static files (build folder)
 app.use(express.static("build"));
 app.use("/auth", userRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html')); // Use path here
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html')); // Serve index.html for other routes
 });
 
+// Connect to MongoDB
 mongoose.connect('mongodb://jaff:mdb5550140@mongo-service:27017/Client?authSource=admin')
-
-
   .then(() => console.log('DB Connected...'))
-  .catch((err) => { console.log(err); });
+  .catch((err) => { console.log('MongoDB connection error:', err); });
 
+// Start the server
 app.listen(port, () => console.log("Server running on port:", port));
+
+
+
+
+
+
 
 
 
@@ -68,8 +70,8 @@ app.listen(port, () => console.log("Server running on port:", port));
 // //app.use(express.static("build"));
 // app.use("/auth", userRouter);
 
-
-
+//mongodb+srv://ajsengineer:mdb5550140@clients.dzqygjh.mongodb.net/?retryWrites=true&w=majority&appName=Clients
+//mongodb://jaff:mdb5550140@mongo-service:27017/Client?authSource=admin
 // mongoose.connect('mongodb+srv://ajsengineer:mdb5550140@clients.dzqygjh.mongodb.net/Clients?retryWrites=true&w=majority').then(() => console.log('DB Connected...')).catch((err) => {console.log(err);})
 
 
